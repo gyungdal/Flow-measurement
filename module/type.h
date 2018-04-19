8d644e144ee7134161194e157cab2ccba9ece5df
Gyeongsik Kim <admin@gyungdal.cc>
2018-04-19 10:25:46 +0900

    한국어 대응 헤더 추가
    xbm 포맷

--- module/type.h
+++ module/type.h
@@ -79,8 +79,28 @@ typedef enum {
 typedef enum {
     uint8_t width;
     uint8_t height;
-    uint8_t value[130];
-} xbm_data_t;
+    uint8_t* value;
+} xbm_t;
+
+typedef enum {
+    CLEAR_COUNT_XBM, //누적량 초기화
+    AMOUNT_BY_DAY_XBM, //날짜별 음수량
+    COUNT_XBM, //누적량
+    WATER_OVERFLOW_XBM, //물량 과다
+    SCALE_XBM, //비율
+    SAVE_AFTER_SETTING_XBM, //설정 후 저장
+    SENSOR_TYPE_XBM, //센서 종류
+    INJECTION_PER_HOUR_XBM, //시간당 주입
+    PER_HOUR_XBM, //시간당
+    NO_XBM, //아니오
+    LOADING_DRUG_XBM, //약물 장전 중
+    YES_XBM, //예
+    MEASURE_ONLY_WATER_XBM, //음수량만 측정
+    SET_CURRENT_TIME_XBM, //현재 시간 설정
+    WATER_SPEED_XBM, //유속
+    CHECK_TUBE_XBM, //튜브 점검
+    STOP_XBM, //정지
+} xbm_type_t;
 
 const uint32_t scaleList[] = {
     33, 35, 40, 45, 50, 60, 70, 80, 90,
